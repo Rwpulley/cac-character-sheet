aa4b8b967d421d0258f081517d427c01cfa8f198
Rhet <108885804+Rwpulley@users.noreply.github.com>
2026-01-30 15:07:27 -0600

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,7 +3,7 @@
 export { CharacterProvider, useCharacter } from './useCharacter';
 export { useCalculations } from './useCalculations';
 export { useToast } from './useToast';
-export { useTheme } from './useTheme';
+export { ThemeProvider, useTheme } from './useTheme';
 
 // Re-export types used by hooks
 export type { Toast } from './useToast';
